9fc044dc3de4af0fff3950ac7065234266db8b98
maailmankaikkeus <wondex@sibmail.com>
2021-01-20 22:33:49 +0700

    2nd Spring Boot Application added in tracker-core module on localhost's port 9090.

--- tracker-core/src/main/java/jdev/tracker/app/TrackerMain.java
+++ tracker-core/src/main/java/jdev/tracker/app/TrackerMain.java
@@ -1,23 +1,23 @@
 package jdev.tracker.app;
 
-import jdev.tracker.TrackerContext;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-public class TrackerMain {
-
-    public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(TrackerContext.class);
-    }
-
-}
-
-//@SpringBootApplication
-//@ComponentScan({"jdev/tracker", "jdev/tracker/services"})
 //public class TrackerMain {
 //
 //    public static void main(String[] args) {
-//        SpringApplication.run(TrackerMain.class, args);
+//        ApplicationContext context = new AnnotationConfigApplicationContext(TrackerContext.class);
 //    }
 //
 //}
+
+@SpringBootApplication
+@ComponentScan({"jdev/tracker", "jdev/tracker/services"})
+public class TrackerMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackerMain.class, args);
+    }
+
+}
