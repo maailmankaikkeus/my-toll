6a587ab6dcc6f325b5b08ff7a90d4a6a3cc83b7d
maailmankaikkeus <wondex@sibmail.com>
2021-02-10 01:43:28 +0700

    In Server-core module: 1. receiveWrappedTrackingList() controller was changed dumping track points data from console log to H2 SpringBoot JPA DB 'server.mv.db'. 2. getLastTrackPoints() GET-controller added, which allows retrieving N last records from DB in the form of TrackDTO JSON, using HTTP-request like "/download?num=N".

--- server-core/src/main/java/jdev/server/app/ServerMain.java
+++ server-core/src/main/java/jdev/server/app/ServerMain.java
@@ -1,15 +1,26 @@
 package jdev.server.app;
 
+import jdev.server.dao.TrackPoint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan({"jdev.server.controllers"})
+@EnableJpaRepositories("jdev.server.dao")
+@EntityScan(basePackageClasses = TrackPoint.class)
 public class ServerMain {
 
     public static void main(String[] args) {
         SpringApplication.run(ServerMain.class, args);
     }
 
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 }
--- server-core/src/main/java/jdev/server/controllers/TrackerController.java
+++ server-core/src/main/java/jdev/server/controllers/TrackerController.java
@@ -3,11 +3,12 @@ package jdev.server.controllers;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import jdev.dto.PointDTO;
 import jdev.dto.TrackDTO;
+import jdev.server.dao.TrackPoint;
+import jdev.server.dao.repo.TrackPointsRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,12 +18,41 @@ public class TrackerController {
 
     private static final Logger log = LoggerFactory.getLogger(TrackerController.class);
 
+    private Iterable<TrackPoint> all;
+
+    @Autowired
+    TrackPointsRepository trackPointsRepository;
+
     @PostMapping(value = "/upload")
     public TrackDTO receiveWrappedTrackingList(@RequestBody TrackDTO track)
             throws JsonProcessingException {
 
+        for (PointDTO item : track.getTrack()) {
+            TrackPoint trackPoint = create(item);
+        }
+
         extractJsonPoints(track.getTrack()).forEach(log::info);
         return track;
+    }
+
+    @GetMapping(value = "/download")
+    private TrackDTO getLastTrackPoints (@RequestParam(value = "last") String num) throws JsonProcessingException {
+        long count = trackPointsRepository.count();
+        List<Integer> idsList = new ArrayList<>();
+        for (long i = count-Long.parseLong(num)+1; i<=count; i++)
+            idsList.add((int)i);
+        all = trackPointsRepository.findAll(idsList);
+        log.info(all.toString());
+        ArrayList<PointDTO> pointList = new ArrayList<PointDTO>() {{
+            for (TrackPoint item : all)
+                add(new PointDTO(
+                        item.getLatitude(),
+                        item.getLongitude(),
+                        item.getAutoId(),
+                        item.getTimeStamp()
+                ));
+        }};
+        return new TrackDTO(pointList);
     }
 
     private List<String> extractJsonPoints(ArrayList<PointDTO> points) throws JsonProcessingException {
@@ -30,5 +60,14 @@ public class TrackerController {
         for (PointDTO p: points)
             pointList.add(p.toJson());
         return pointList;
+    }
+
+    private TrackPoint create(PointDTO point) {
+        TrackPoint trackPoint = new TrackPoint();
+        trackPoint.setLatitude(point.getLat());
+        trackPoint.setLongitude(point.getLon());
+        trackPoint.setAutoId(point.getAutoId());
+        trackPoint.setTimeStamp(point.getTime());
+        return trackPointsRepository.save(trackPoint);
     }
 }
